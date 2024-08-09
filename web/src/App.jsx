a554279917c5310d8ee1d2e6d4151fd632343ad4
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-09 08:09:29 -0300

    :wastebasket: remove: Removendo componentes padrão do react

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,39 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { collection, getDocs } from 'firebase/firestore';
-import { useEffect } from 'react';
-import { db } from './firebaseDb/firebaseDb';
+import React from 'react';
 
 function App() {
-
-  const productsCollectionRef = collection(db, "products");
-
-  useEffect(() => {
-    const getProducts = async () => {
-      const data = await getDocs(productsCollectionRef);
-      console.log(data);
-    };
-
-    getProducts();
-  }, [productsCollectionRef]);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <header>
+          <p>Hellow World</p>
+        </header>
+      </div>
   );
 }
 
