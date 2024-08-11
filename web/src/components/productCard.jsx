6f3ab7326438d1461870b14a818ff29e3e1fb784
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-11 19:15:04 -0300

    :truck: chore: movendo função e botão de adicionar ao carrinho

--- web/src/components/productCard.jsx
+++ web/src/components/productCard.jsx
@@ -1,22 +1,8 @@
 import React from "react";
-import { useContext } from 'react';
-import { GlobalStateContext } from "../context/context";
-import { useNavigate } from "react-router-dom";
-// import mockDataBase from "../mock/mock";
 
 function ProductCard({image, description, price, index}) {
-    const { cart, setCart } = useContext(GlobalStateContext);
     const temporaryImageSize = { width: "300px", height: "300px"};
     
-    const addProduct = (image, description, price) => {
-        const productObjet = {
-            image,
-            description,
-            price
-        }
-        const updateCart = [...cart, productObjet];
-        setCart(updateCart);
-    };
     return(
         <div style={{backgroundColor: "withe"}}>
                     <div key={`product-id${index}`}>
@@ -36,9 +22,6 @@ function ProductCard({image, description, price, index}) {
                             <p>
                                 {price.toFixed(2)}
                             </p>
-                        </div>
-                        <div>
-                            <input type='button' value="Comprar" onClick={() => addProduct(image, description, price)}/>
                         </div>
                     </div>
         </div>
