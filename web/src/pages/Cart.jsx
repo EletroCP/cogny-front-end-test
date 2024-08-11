a4a867cf915f9e0e747b9dad6ff1b7feebd22e42
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-11 19:32:42 -0300

    :wrench: chore: Adicionando metodo de organização dos produtos iguais no carrinho

--- web/src/pages/Cart.jsx
+++ web/src/pages/Cart.jsx
@@ -1,27 +1,49 @@
 import React from "react";
 import { useContext } from 'react';
 import { GlobalStateContext } from "../context/context";
-import ProductCard from "../components/productCard";
 import Logo from "../components/Logo";
 import MyCart from "../components/MyCart";
 
 function Cart() {
     const { cart } = useContext(GlobalStateContext);
+
+    const groupedCart = cart.reduce((acc, item) => {
+        const key = `${item.image}-${item.description}-${item.price}`;
+        if (!acc[key]) {
+            acc[key] = { ...item, quantity: 0 };
+        }
+        acc[key].quantity += 1;
+        return acc;
+    }, {});
+
+    const cartItems = Object.values(groupedCart);
     
     return(
         <div>
             <Logo url="/"/>
             <MyCart url="/cart"/>
-            {cart.map(({image, description, price}, index) => (
-                    <ProductCard 
-                        key={`product-id${index}`}
-                        image={image} 
-                        description={description} 
-                        price={price}
-                    />
+            {cartItems.map(({ image, description, price, quantity }, index) => (
+                <div key={`product-id${index}`} style={{ backgroundColor: "white", padding: "10px", margin: "10px" }}>
+                    <div>
+                        <img
+                            src={image}
+                            alt={description}
+                            style={{ width: "300px", height: "300px" }}
+                        />
+                    </div>
+                    <div>
+                        <p>{description}</p>
+                    </div>
+                    <div>
+                        <p>{price.toFixed(2)}</p>
+                    </div>
+                    <div>
+                        <p>Quantidade: {quantity}</p>
+                    </div>
+                </div>
             ))}
         </div>
-    )
+    );
 }
 
 export default Cart;
