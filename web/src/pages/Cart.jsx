7eb158eb64194fdcad86d6f680d7552d7f02b87b
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-11 18:34:00 -0300

    :bricks: ci: Componentizando a pagina Cart

--- web/src/pages/Cart.jsx
+++ web/src/pages/Cart.jsx
@@ -2,39 +2,23 @@ import React from "react";
 import { useContext } from 'react';
 import { GlobalStateContext } from "../context/context";
 import { useNavigate } from "react-router-dom";
+import ProductCard from "../components/productCard";
 
 function Cart() {
     const { cart } = useContext(GlobalStateContext);
-    const temporaryImageSize = { width: "300px", height: "300px"};
     const navigate = useNavigate();
     
     return(
         <div>
             <p onClick={() => navigate("/")}>Home</p>
-            {cart.map(({image, description, price}, index) => {
-                return(
-                    <div key={`product-id${index}`}>
-                        <p style={{color:"white"}}>Cart</p>
-                        <div >
-                            <img
-                                src={image}
-                                alt={description}
-                                style={temporaryImageSize}
-                            />
-                        </div>
-                        <div>
-                            <p>
-                                {description}
-                            </p>
-                        </div>
-                        <div>
-                            <p>
-                                {price}
-                            </p>
-                        </div>
-                    </div>
-                )
-            })}
+            {cart.map(({image, description, price}, index) => (
+                    <ProductCard 
+                        key={`product-id${index}`}
+                        image={image} 
+                        description={description} 
+                        price={price}
+                    />
+            ))}
         </div>
     )
 }
