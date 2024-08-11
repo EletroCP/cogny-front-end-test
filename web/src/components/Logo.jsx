8c4ccf9b4156a5a3551747fcbe73f6d0a5733152
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-11 19:01:20 -0300

    :sparkles: feat: componente de logo

---
+++ web/src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import shoes from "../icons/shoes.jpg"
+import { useNavigate } from "react-router-dom";
+const Logo = ({url}) => {
+    const navigate = useNavigate()
+    return(
+        <div onClick={() => navigate(url)}>
+            <h2>COGNYSHOES</h2>
+            <img style={{width:"100px", height: "100px"}} alt="logo" src={shoes}/>
+        </div>
+    )
+}
+
+export default Logo;
