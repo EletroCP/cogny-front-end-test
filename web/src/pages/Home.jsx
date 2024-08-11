86abc7256ff5fa97c31b1e3c1121c0032b9ba2e1
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-11 19:16:52 -0300

    :sparkles: feat: adicionando função de adicionar ao carrinho, logo e MyCart

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -3,21 +3,38 @@ import { useContext } from "react";
 import { GlobalStateContext } from "../context/context";
 import ProductCard from "../components/productCard";
 import Logo from "../components/Logo";
+import MyCart from "../components/MyCart";
 
 const Home = () => {
-    const { productsDb } = useContext(GlobalStateContext);
-    // const productsDb = mockDataBase;
+    const { productsDb, cart, setCart } = useContext(GlobalStateContext);
 
+    const addProduct = (image, description, price) => {
+        const productObjet = {
+            image,
+            description,
+            price
+        }
+        const updateCart = [...cart, productObjet];
+        setCart(updateCart);
+    };
     return(
         <div>
-            <Logo url={"/cart"}/>
+            <div>
+                <Logo url={"/"}/>
+                <MyCart url={"/cart"}/>
+            </div>
             {productsDb.map(({image, description, price}, index) => (
+                <div key={`product-id${index}`}>
                     <ProductCard 
                         key={`product-id${index}`}
                         image={image} 
                         description={description} 
                         price={price}
                     />
+                    <div>
+                        <input type='button' value="Comprar" onClick={() => addProduct(image, description, price)}/>
+                    </div>
+                </div>
             ))}
         </div>
     );
