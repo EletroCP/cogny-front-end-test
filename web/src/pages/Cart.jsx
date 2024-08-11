2c700b82a55e8c251e2be66ccc5c36efe390574d
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-11 19:17:24 -0300

    :sparkles: feat: adicionando Logo ao carrinho

--- web/src/pages/Cart.jsx
+++ web/src/pages/Cart.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useContext } from 'react';
 import { GlobalStateContext } from "../context/context";
-import { useNavigate } from "react-router-dom";
 import ProductCard from "../components/productCard";
 import Logo from "../components/Logo";
+import MyCart from "../components/MyCart";
 
 function Cart() {
     const { cart } = useContext(GlobalStateContext);
-    const navigate = useNavigate();
     
     return(
         <div>
             <Logo url="/"/>
+            <MyCart url="/cart"/>
             {cart.map(({image, description, price}, index) => (
                     <ProductCard 
                         key={`product-id${index}`}
