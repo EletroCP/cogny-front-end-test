7382cf083b01e6ae5890b975b3e13fac2a9ddf99
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-12 10:47:30 -0300

    :lipistick: feat: estilizando logo

--- web/src/components/Logo.jsx
+++ web/src/components/Logo.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import shoes from '../icons/shoes.png'
 import { useNavigate } from 'react-router-dom';
+import '../style/Logo.css';
+
 const Logo = ({url}) => {
     const navigate = useNavigate()
     return(
-        <div onClick={() => navigate(url)}>
-            <h2>COGNYSHOES</h2>
-            <img style={{width:'100px', height: '100px'}} alt='logo' src={shoes}/>
+        <div onClick={() => navigate(url)} id='header-container'>
+            <h2 id='header-title'>COGNYSHOES</h2>
+            <img id='header-logo' alt='logo' src={shoes}/>
         </div>
     )
 }
