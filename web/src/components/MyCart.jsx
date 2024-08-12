14c9e410c4be8be054d415f3e250d941367b1013
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-12 11:33:55 -0300

    :lipistick: feat: estilizando MyCart

--- web/src/components/MyCart.jsx
+++ web/src/components/MyCart.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { useContext } from 'react';
 import { GlobalStateContext } from '../context/context';
 import { useNavigate } from 'react-router-dom';
+import '../style/MyCart.css';
+
 const MyCart = ({url}) => {
     const { cart } = useContext(GlobalStateContext);
     const navigate = useNavigate();
     return(
-        <div onClick={() => navigate(url)}>
-            <p>Meu carrinho</p>
-            <p>{`${cart.length} itens`}</p>
+        <div onClick={() => navigate(url)} id='header-mycart-container'>
+            <h4 id='header-mycart-text'>Meu carrinho</h4>
+            <p id='header-mycart-itens'>{`${cart.length} itens`}</p>
         </div>
     )
 };
