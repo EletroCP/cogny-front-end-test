bc79757c1a7649351c88757a54cd3a158bbb0db3
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-13 11:45:35 -0300

    :lipstick: feat: adicionando IDs e class names

--- web/src/components/EmptyCart.jsx
+++ web/src/components/EmptyCart.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import '../style/EmptyCart.css'
 
 const EmptyCart = () => {
     return(
-        <div>
-            <p>Seu carrinho está vazio.</p>
-            <p>Voltar para loja.</p>
+        <div id='main-empty-cart-container'>
+            <p id='empty-cart-main-text' className='text-empty-cart'>Seu carrinho está vazio.</p>
+            <p id='back-to-store' className='text-empty-cart'>Voltar para loja.</p>
         </div>
     );
 };
