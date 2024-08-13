78ec7748e81bfc08c7da00ebed04f30e2b08bdc9
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-13 12:45:15 -0300

    :lipstick: feat: passando IDs e estado

--- web/src/components/SpamBuy.jsx
+++ web/src/components/SpamBuy.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import '../style/SpamBuy.css';
+import { GlobalStateContext } from "../context/context";
 
 const SpamBuy = () => {
+    const {setFinishPurchase, setCart } = useContext(GlobalStateContext);
     return(
-    <spam>
-        <p>Sua compra foi finalizada</p>
-        <input type='button' value='Fechar'/>
-    </spam>
+    <div id='spam-main-container'>
+        <p id='spam-main-text'>Sua compra foi finalizada</p>
+        <input
+            id='spam-main-button'
+            type='button'
+            value='Fechar'
+            onClick={() =>{ setFinishPurchase(false); setCart([]); }}/>
+    </div>
     );
 }
     export default SpamBuy;
