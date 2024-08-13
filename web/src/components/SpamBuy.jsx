69f9ef23b8964f031dbca5cb195468d9167374bf
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-13 11:13:23 -0300

    :sparkles: feat: Span de finalização de compra

---
+++ web/src/components/SpamBuy.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SpamBuy = () => {
+    return(
+    <spam>
+        <p>Sua compra foi finalizada</p>
+        <input type='button' value='Fechar'/>
+    </spam>
+    );
+}
+    export default SpamBuy;
