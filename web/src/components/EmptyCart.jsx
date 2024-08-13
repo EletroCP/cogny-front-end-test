1ead0914819447b53def4933c8a45bc6fd23b338
Breno L. Lopes <brernolopes@hotmail.com>
2024-08-13 12:46:48 -0300

    :sparkles: feat: botão para voltar a loja

--- web/src/components/EmptyCart.jsx
+++ web/src/components/EmptyCart.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import '../style/EmptyCart.css'
 
 const EmptyCart = () => {
+    const navigate = useNavigate()
     return(
         <div id='main-empty-cart-container'>
             <p id='empty-cart-main-text' className='text-empty-cart'>Seu carrinho está vazio.</p>
-            <p id='back-to-store' className='text-empty-cart'>Voltar para loja.</p>
+            <input
+                type='button'
+                value='Voltar para loja'
+                id='back-to-store'
+                className='text-empty-cart'
+                onClick={() => navigate('/')} />
         </div>
     );
 };
